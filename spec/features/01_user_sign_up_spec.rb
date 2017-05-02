77e5b70cac1556181087f13de23299717d1d1ea3
John Duggan <JLDuggan87@gmail.com>
2017-05-02 11:57:02 -0400

    fix codeship errors

--- spec/features/01_user_sign_up_spec.rb
+++ spec/features/01_user_sign_up_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'User signs up' do
   scenario 'user signs up succesfully' do
     visit new_user_registration_path
-
       fill_in 'First name', with: 'Frank'
       fill_in 'Last name', with: 'Tank'
       fill_in 'User name', with: 'frankthetank'
@@ -11,6 +10,5 @@ feature 'User signs up' do
       fill_in 'Password', with: 'password123'
       fill_in 'Password confirmation', with: 'password123'
       click_on 'Sign up'
-
   end
 end
