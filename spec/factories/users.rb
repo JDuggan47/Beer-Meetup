ea79df18c358ec62a2bee572550163c9652f0855
John Duggan <JLDuggan87@gmail.com>
2017-05-02 11:28:15 -0400

    add user sign in test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:first_name) { |n| "Frank #{n}" }
+    sequence(:last_name) { |n| "Tank #{n}" }
+    sequence(:username) { |n| "frankthetank#{n}" }
+    sequence(:email) { |n| "frank#{n}@thetank.com" }
+    password 'password'
   end
 end
