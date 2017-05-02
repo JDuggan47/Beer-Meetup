34346d9d36e534c04d887c10bde3154306f46be4
John Duggan <JLDuggan87@gmail.com>
2017-05-02 15:45:57 -0400

    add beers  table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502132138) do
+ActiveRecord::Schema.define(version: 20170502193620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string   "name_of_beer",    null: false
+    t.string   "name_of_brewery", null: false
+    t.string   "style",           null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,7 +38,7 @@ ActiveRecord::Schema.define(version: 20170502132138) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "user_name"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
