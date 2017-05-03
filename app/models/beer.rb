93a7a57dac52fbd1a491ff9592926e24a42d8458
John Duggan <JLDuggan87@gmail.com>
2017-05-03 09:25:02 -0400

    fix user login

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,7 @@
 class Beer < ApplicationRecord
-  belongs_to :users
+  validates :name_of_beer, presence: true
+  validates :name_of_brewery, presence: true
+  validates :style, presence: true
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,8 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :beers
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
